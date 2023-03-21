8334d1b833877ea8810a944e6c25841380d1219d
Krati Jain <kratijain88@gmail.com>
2023-03-21 16:57:55 +0530

    Pushed Test Project

---
+++ LDKTest/Program.cs
@@ -0,0 +1,8 @@
+﻿using LibrtyDevKit;
+
+LibrtyDevKit.LibrtyDevKit LDK = new LibrtyDevKit.LibrtyDevKit("LibrtyDubaiHack");
+// Token APIs
+var response = await LDK.getTokenBalance("0xc2132d05d31c914a87c6611c10748aeb04b58e8f", "0x6fe489d80a48caf7472cd45f4258b9ffb5208e3c", LDKNetworkType.polygon);
+Console.WriteLine($"Data: {response.data}");
+Console.WriteLine($"status: {response.status}");
+Console.WriteLine($"message: {response.message}");
